7e76a848f1105315c41c31a9db87b46daae544d4
Neil Ruud <neil@ruudmedia.com>
2022-10-01 12:12:44 -0700

    remove unsed  lib

--- models.py
+++ models.py
@@ -1,8 +1,6 @@
 from sqlalchemy import Column, DateTime, String, Float
 from sqlalchemy.ext.declarative import declarative_base
 
-from sqlalchemy.orm import relationship
-
 Base = declarative_base()
 
 
